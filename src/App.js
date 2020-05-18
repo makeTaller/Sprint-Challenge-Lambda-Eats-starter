bb72e661b4fd02fed4e178aa41478deaca1abfcd
Kirk Tolliver <kirktolliver28@gmail.com>
2020-05-18 16:10:24 -0500

    Made Changes to Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {Route, Link} from "react-router-dom";
+import Home from "./Home"
 
 const App = () => {
   return (
     <>
+        <Home />
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-      <Link to={`/pizza/${pizzaForm.id}`}>
+      {/* <Link to={`/`}> */}
       <button>Create Pizza</button>
+      {/* </Link> */}
     </>
   );
 };
