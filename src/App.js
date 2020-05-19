afe75d92118129519038e03bc383e2561963fa85
Kirk Tolliver <kirktolliver28@gmail.com>
2020-05-18 19:07:38 -0500

    Adjusing home components.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
-import {Route, Link} from "react-router-dom";
-import Home from "./Home"
+import {Route,Switch, Link} from "react-router-dom";
+import Home from "./Home";
+import PizzaForm from "./components/PizzaForm.js";
 
 const App = () => {
   return (
     <>
+        <Link to="/">
         <Home />
+        </Link to="pizzaform">
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
       {/* <Link to={`/`}> */}
       <button>Create Pizza</button>
       {/* </Link> */}
+      <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/pizzaform" component={PizzaForm} />
+      </Switch>
     </>
   );
 };
